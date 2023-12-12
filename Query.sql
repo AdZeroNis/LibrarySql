b9aff39d4532af8e3a9a080f8bff5748ec258423
shghygh001 <shghyghkrimi2923@gmail.com>
2023-12-12 10:42:58 +0330

    Select Query

--- Query.sql
+++ Query.sql
@@ -13,6 +13,20 @@ SELECT *
 FROM book
 INNER JOIN publisher
 ON book.PublisherName = publisher.PublisherName;
-
+ 
+SELECT *
+FROM borrower
+RIGHT JOIN bookLoans
+ON borrower.CardNo=bookLoans.CardNo;
 
 SELECT PublisherName FROM publisher WHERE PublisherName IN (SELECT PublisherName FROM bookLoans WHERE BookID = 1  AND PublisherName IN ('Negah'));
+
+SELECT BorrowerName  FROM borrower WHERE BorrowerName  IN (SELECT BorrowerName FROM bookLoans WHERE CardNo=1)
+
+
+	select * from publisher
+	select * from book
+	select * from borrower
+	select * from bookLoans
+	select * from copies
+	select * from libraryBranch
