7e98a7314c44cbabf863c854833e410a5ae708e0
shghygh001 <shghyghkrimi2923@gmail.com>
2023-12-15 17:33:23 +0330

    Add new Sp

--- SP.sql
+++ SP.sql
@@ -22,6 +22,30 @@ Exec Gender 0, @res output
 print(@res)
 
 
+CREATE PROCEDURE GetBorrowerDetails
+@CardNo INT,
+@BorrowerName VARCHAR(100) OUTPUT,
+@BorrowerAddress VARCHAR(200) OUTPUT,
+@BorrowerPhone VARCHAR(50) OUTPUT,
+@Gender BIT OUTPUT
+AS
+BEGIN
+ SELECT @BorrowerName = BorrowerName, @BorrowerAddress = BorrowerAddress, @BorrowerPhone = BorrowerPhone, @Gender = Gender
+ FROM borrower
+ WHERE CardNo = @CardNo
+END
+
+DECLARE @BorrowerName VARCHAR(100), @BorrowerAddress VARCHAR(200), @BorrowerPhone VARCHAR(50), @Gender BIT
+EXEC GetBorrowerDetails 1, @BorrowerName OUTPUT, @BorrowerAddress OUTPUT, @BorrowerPhone OUTPUT, @Gender OUTPUT
+PRINT @BorrowerName
+PRINT @BorrowerAddress
+PRINT @BorrowerPhone
+PRINT @Gender
+
+
+
+
+
 
 
 
