f01a9bc2940015229d1b70afd9d42825fd031232
shghygh001 <shghyghkrimi2923@gmail.com>
2023-12-18 23:18:34 +0330

    Add Tabe

--- Tavabe.sql
+++ Tavabe.sql
@@ -1,7 +1,10 @@
 SELECT SUBSTRING(Title, 1, 3) AS ShortTitle FROM book;
 SELECT SUBSTRING(PublisherName, 1, 5) AS ShortPublisherName FROM publisher;
 
-SELECT CHARINDEX('abcd', PublisherName, 10) AS Position FROM publisher;
+SELECT LTRIM(PublisherName) AS TrimmedPublisherName FROM publisher;
+SELECT LTRIM(Title) AS TrimmedTitle FROM book;
+
+
 
 
 select * from publisher
