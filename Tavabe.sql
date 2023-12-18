cdda5190a55d4560b4a2aeee7ced3cd154360de3
shghygh001 <shghyghkrimi2923@gmail.com>
2023-12-18 23:18:13 +0330

    Add Tabe

--- Tavabe.sql
+++ Tavabe.sql
@@ -1,3 +1,12 @@
 SELECT SUBSTRING(Title, 1, 3) AS ShortTitle FROM book;
 SELECT SUBSTRING(PublisherName, 1, 5) AS ShortPublisherName FROM publisher;
 
+SELECT CHARINDEX('abcd', PublisherName, 10) AS Position FROM publisher;
+
+
+select * from publisher
+select * from book
+select * from borrower
+select * from bookLoans
+select * from copies
+select * from libraryBranch
